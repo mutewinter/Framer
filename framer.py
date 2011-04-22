23b7dd027158f250bad4704295fb710a569fe4f9
Jeremy Mack <jeremy.mack13@gmail.com>
2011-04-21 21:53:45 -0400

    Switched to open source iPad 2 images Updated to work with new Acorn formatted resources Updated readme to remove links to Apple resources

--- framer.py
+++ framer.py
@@ -5,12 +5,10 @@
 # Company: Pile of Turtles, LLC
 # Company Website: http://pileofturtles.com
 
-# Acorn script that opens a target screenshot and applies the iPad frame It
-# accepts one argument, which is the location of the PNG file to which it is
-# going to add the screenshot.
+# Wrapper for framer.jstalk to call it for a set of image files in a given
+# directory.
 
 import glob
-#import sys
 import os
 import lib.argparse as argparse
 from subprocess import call
@@ -18,6 +16,8 @@ from subprocess import call
 jstalk = "jstalk"
 framerJS = "framer.jstalk"
 outputFolderName = "framed"
+script_folder = os.path.dirname(os.path.abspath(__file__))
+resources_directory = os.path.join(script_folder, "resources/")
 
 def parse_commandline_arguments():
   parser = argparse.ArgumentParser(description=
@@ -31,28 +31,26 @@ def parse_commandline_arguments():
       help='The directory to process')
 
 
-
   return parser.parse_args()
   
 
-
 def process_directory(directory):
   if directory:
+    print "Getting my frame on."
+
     outputFolder = os.path.join(directory, outputFolderName) 
     if not os.path.exists(outputFolder):
-      print "Created output directory"
+      print "framed folder created"
       os.makedirs(outputFolder)
     
 
-    print "Parsing images in " + directory
+    pngFiles = glob.glob(directory + '*.png')
 
-    for pngFile in glob.glob(directory + '*.png'):
-      call([jstalk, framerJS, pngFile])
+    for pngFile in pngFiles:
+      call([jstalk, framerJS, pngFile, resources_directory])
 
-    print "Parsing complete, files saved in:"
-    print outputFolder
+    print "Framed %d images!" % len(pngFiles)
 
 if __name__ == '__main__':
   args = parse_commandline_arguments()
-
   process_directory(args.directory[0])
